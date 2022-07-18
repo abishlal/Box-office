aca81c5467dc60c2107ba7eea654a51d638f9a2e
Abishlal <abishlalns03@gmail.com>
2022-07-18 19:33:47 +0530

    init commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Mainpagelayout from '../components/Mainpagelayout'
+
+const Home = () => {
+  return (
+    <Mainpagelayout>Home</Mainpagelayout>
+  )
+}
+
+export default Home
---
+++ src/pages/Starred.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Mainpagelayout from '../components/Mainpagelayout'
+
+const Starred = () => {
+  return (
+    <Mainpagelayout>Starred</Mainpagelayout>
+  )
+}
+
+export default Starred
